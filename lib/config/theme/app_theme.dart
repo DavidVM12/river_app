b0ae80a85622020161bd4eebf121d86d13bf6e44
David Santiago Velasco <david.velasco@celuweb.com>
2023-07-26 09:29:49 -0500

    feat: first implementation of GoRouter

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+const seedColor = Color.fromARGB(255, 7, 80, 59);
+
 class AppTheme {
-  ThemeData getTheme() => ThemeData();
+  ThemeData getTheme() => ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: seedColor,
+      listTileTheme: const ListTileThemeData(
+        iconColor: seedColor,
+      ));
 }
